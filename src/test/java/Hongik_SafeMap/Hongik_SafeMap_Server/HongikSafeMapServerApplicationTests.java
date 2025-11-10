cd215cbff36b0f3cc9f7e071365a6ec43adf4a00
Fluteen <east03290@gmail.com>
2025-11-10 14:43:40 +0900

    first commit

---
+++ src/test/java/Hongik_SafeMap/Hongik_SafeMap_Server/HongikSafeMapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Hongik_SafeMap.Hongik_SafeMap_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongikSafeMapServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
