40686842a352adb56809a563284445bf6aaa41d5
seoyeon5117 <seoyun020207@gmail.com>
2026-03-28 16:54:06 +0900

    feat: 재난제보 그룹 리포지토리 구현

--- core-api/src/main/java/Hongik_SafeMap_Server/domain/disaster_report/repository/DisasterReportRepository.java
+++ core-api/src/main/java/Hongik_SafeMap_Server/domain/disaster_report/repository/DisasterReportRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -40,18 +41,25 @@ public interface DisasterReportRepository extends JpaRepository<DisasterReport, 
     Page<DisasterReport> findByDisasterTypeInAndRiskLevelInOrderByCreatedAtDesc(List<DisasterType> disasterTypes, List<RiskLevel> riskLevels, Pageable pageable);
 
     @Query("""
-    select new Hongik_SafeMap_Server.domain.admin.member.dto.AdminMemberResponse(
-        m.id,
-        m.name,
-        m.email,
-        count(dr),
-        0,
-        m.isCredible
-    )
-    from Member m
-    left join DisasterReport dr on dr.member = m
-    where m.status = Hongik_SafeMap_Server.vo.MemberStatus.USER
-    group by m.id, m.name, m.email, m.isCredible
-""")
+                select new Hongik_SafeMap_Server.domain.admin.member.dto.AdminMemberResponse(
+                    m.id,
+                    m.name,
+                    m.email,
+                    count(dr),
+                    0,
+                    m.isCredible
+                )
+                from Member m
+                left join DisasterReport dr on dr.member = m
+                where m.status = Hongik_SafeMap_Server.vo.MemberStatus.USER
+                group by m.id, m.name, m.email, m.isCredible
+            """)
     List<AdminMemberResponse> findAdminMemberList();
+
+    // 그룹 아이디로 제보 목록 조회
+    List<DisasterReport> findByGroupId(@Param("groupId") Long groupId);
+
+    // 여러 그룹의 제보들 조회
+    @Query("SELECT dr FROM DisasterReport dr WHERE dr.group.id IN :groupIds")
+    List<DisasterReport> findByGroupIdIn(@Param("groupIds") List<Long> groupIds);
 }
