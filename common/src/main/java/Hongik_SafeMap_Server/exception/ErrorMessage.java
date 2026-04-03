003e8d4092024c59f7d787a3231ead18ca4b9bed
seoyeon5117 <seoyun020207@gmail.com>
2026-04-04 02:13:35 +0900

    feat: 행동 요령 에러 메시지 오타 수정

--- common/src/main/java/Hongik_SafeMap_Server/exception/ErrorMessage.java
+++ common/src/main/java/Hongik_SafeMap_Server/exception/ErrorMessage.java
@@ -33,8 +33,7 @@ public class ErrorMessage {
     public static final String RESOURCE_REPORT_NOT_FOUND = "해당 id를 가진 자원 게시글을 찾을 수 없습니다.";
 
     // SafetyTip
-    public static final String SAFETY_TIP_NOT_FOUND = "해당 재난 유형을 수 없습니다.";
-    public static final String SAFETY_TIP_ALREADY_EXISTS = "해당 재난 유형의 안전 수칙이 이미 존재합니다.";
+    public static final String SAFETY_TIP_NOT_FOUND = "해당 재난 유형을 찾을 수 없습니다.";
 
     // 게시물 공통
     public static final String REPORT_DELETE_UNAUTHORIZED = "본인이 작성한 게시물만 삭제할 수 있습니다";
