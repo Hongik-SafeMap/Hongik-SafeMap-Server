e35bca142e28ae8f58d19c96c8699311738a03cf
SeoYeon Choi <143925634+seoyeon5117@users.noreply.github.com>
2026-04-06 16:29:02 +0900

    fix: 관리자 권한 오류 해결 (#53)
    - @JsonValue로 인해 jwt 토큰에 ROLE_관리자 저장됨

--- common/src/main/java/Hongik_SafeMap_Server/vo/MemberStatus.java
+++ common/src/main/java/Hongik_SafeMap_Server/vo/MemberStatus.java
@@ -1,6 +1,5 @@
 package Hongik_SafeMap_Server.vo;
 
-import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,6 +9,5 @@ public enum MemberStatus {
     USER("일반"),
     ADMIN("관리자");
 
-    @JsonValue
     private final String description;
 }
